aff4428641660dc2b3ebeb0aa5386bc7022990f1
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-02-11 00:00:12 +0300

    feat: add skill component; add styles for skill, skills

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,28 +1,34 @@
 import { title } from "process";
 import styled from "styled-components";
+import { myTheme } from "../global/MyTheme.styled";
 import { Icon } from "../icon/icon";
 import { SectionTitle } from "../sectionTitle/SectionTitle";
 
 type SkillPropsType = {
   iconId: string;
-  
+  title: string;
 };
 
 export const Skill = (props: SkillPropsType) => {
   return (
     <StyledSkill>
       <Icon iconId={props.iconId} width="auto" height="100px" />
-      {/* <SkillTitle></SkillTitle> */}
+      <SkillTitle>{props.title}</SkillTitle>
     </StyledSkill>
   );
 };
 
 const StyledSkill = styled.div`
-  display: block;
+  
 `;
 
 const SkillTitle = styled.div`
-  border: 1px solid blue;
-  width: 100px;
-  height: 100px;
-`;
+  /* border: 1px solid blue; */
+  max-width: 100px;
+  min-height: 40px;
+  text-align: center;
+  color: ${myTheme.colors.secondary};
+  padding: 10px 0 0 0;
+  /* word-wrap: break-word; */
+  /* overflow-wrap: break-word; */
+`
--- src/layouts/sections/skills/Skills.tsx
+++ src/layouts/sections/skills/Skills.tsx
@@ -11,17 +11,26 @@ export const Skills = () => {
         direction="row"
         wrap="wrap"
         justify="space-between"
-        content="space-between"
+        content="center"
         gapItem="20px"
       >
-        <Skill iconId={"html5SvgrepoCom"}/>
-        <Skill iconId={"gitSvgrepoCom"}/>
-        <Skill iconId={"css3SvgrepoCom"}/>
-        <Skill iconId={"gmailSvgrepoCom"}/>
-        <Skill iconId={"linkedinSvgrepoCom"}/>
-        <Skill iconId={"reduxSvgrepoCom"}/>
-        <Skill iconId={"vscodeSvgrepoCom"}/>
-        <Skill iconId={"bemSvgrepoCom"}/>
+        <Skill iconId={"html5Svg"} title={"HTML"}/>
+        <Skill iconId={"css3Svg"} title={"CSS"}/>
+        <Skill iconId={"sassSvg"} title={"SCSS"}/>
+        <Skill iconId={"tailwindCssSvg"} title={"TailwindCss"}/>
+        <Skill iconId={"javascriptSvgrepoCom"} title={"JavaScript"}/>
+        <Skill iconId={"TypeScript"} title={"TypeScript"}/>
+        <Skill iconId={"reactSvg"} title={"React"}/>
+        <Skill iconId={"reduxSvg"} title={"Redux"}/>
+        <Skill iconId={"styledComponentsSvg"} title={"Styled Component"}/>
+        <Skill iconId={"vscodeSvg"} title={"VS Code"}/>
+        <Skill iconId={"webstormSvg2"} title={"Webstorm"}/>
+        <Skill iconId={"webpackSvg"} title={"Webpack"}/>
+        <Skill iconId={"AISvg"} title={"Artificial Intelligence"}/>
+        <Skill iconId={"NPM"} title={"npm"}/>
+        <Skill iconId={"BemMetodology"} title={"BEM Metodology"}/>
+        <Skill iconId={"githubSvg"} title={"GitHub"}/>
+        <Skill iconId={"gitSvg"} title={"Git"}/>
       </FlexWrapper>
     </StyledSkills>
   );
@@ -31,5 +40,4 @@ const StyledSkills = styled.section`
   max-width: 100%;
   max-height: 100%;
   padding: 20px 0;
-  border: 1px solid pink;
 `;
