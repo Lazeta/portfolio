d270d9429695c48984b79037432ea3c5225ae822
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-04-06 18:22:05 +0300

    refactor: finish same refactoring for HeroSection

---
+++ src/layout/sections/herosection/HeroSection.styles.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { myTheme } from "../../../components/global/MyTheme.styled";
+import { FlexWrapper } from "../../../components/wrapper/FlexWrapper";
+
+const Title = styled.h1`
+  color: ${myTheme.colors.secondary};
+`;
+
+const Paragraph = styled.p`
+  color: ${myTheme.colors.secondary};
+  max-width: 800px;
+  padding: 0 0px 20px 0;
+
+  @media ${myTheme.media.mobile} {
+    max-width: 100%;
+  }
+`;
+
+const HeroSection = styled.section`
+  width: auto;
+  min-height: 250px;
+`;
+
+const CustomFlexWrapper = styled(FlexWrapper)`
+  @media ${myTheme.media.mobile} {
+    gap: 0;
+  }
+`;
+
+const Container = styled.div`
+    Button { 
+        margin-bottom: 20px;
+    }
+`;
+
+const TitleSecond = styled.h2`
+  color: ${myTheme.colors.any};
+  font-size: 4rem;
+  margin: 0;
+`;
+
+export const S = {
+  Title,
+  Paragraph,
+  HeroSection,
+  CustomFlexWrapper,
+  Container,
+  TitleSecond,
+};
--- src/layout/sections/herosection/HeroSection.tsx
+++ src/layout/sections/herosection/HeroSection.tsx
@@ -1,60 +1,24 @@
-import styled from "styled-components";
-import { myTheme } from "../../../components/global/MyTheme.styled";
-import { FlexWrapper } from "../../../components/wrapper/FlexWrapper";
 import { Button } from "../../../components/buttons/Button";
 import { Logo } from "../../../components/logo/Logo";
+import { S } from "./HeroSection.styles";
 
 export const HeroSection = () => {
   return (
-    <StyledHeroSection>
-      <CustomFlexWrapper justify="space-between" gapItem="40px">
-        <div>
-          <StyledTitle>Frontend Developer</StyledTitle>
-          <h2>Chekh Stas</h2>
-          <StyledParagraph>
+    <S.HeroSection>
+      <S.CustomFlexWrapper justify="space-between" gapItem="40px">
+        <S.Container>
+          <S.Title>Frontend Developer</S.Title>
+          <S.TitleSecond>Chekh Stas</S.TitleSecond>
+          <S.Paragraph>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
             obcaecati earum tempore non officiis nisi incidunt sapiente
             temporibus ipsa? Sint quas facilis eum sequi laborum ad nobis nulla
             vitae perspiciatis!
-          </StyledParagraph>
+          </S.Paragraph>
           <Button title={"Contact Me"} width={"150px"} font={"1.2rem"} />
-        </div>
+        </S.Container>
         <Logo />
-      </CustomFlexWrapper>
-    </StyledHeroSection>
+      </S.CustomFlexWrapper>
+    </S.HeroSection>
   );
 };
-
-const CustomFlexWrapper = styled(FlexWrapper)`
-  @media ${myTheme.media.mobile} {
-    gap: 0;
-  }
-`
-
-const StyledHeroSection = styled.section`
-  width: auto;
-  min-height: 250px;
-  > * > * p,
-  .Button {
-    padding: 0 0px 20px 0;
-  }
-
-  > * > * h2 {
-    color: ${myTheme.colors.any};
-    font-size: 4rem;
-    margin: 0;
-  }
-`;
-
-const StyledTitle = styled.h1`
-  color: ${myTheme.colors.secondary};
-`;
-
-const StyledParagraph = styled.p`
-  color: ${myTheme.colors.secondary};
-  max-width: 800px;
-
-  @media ${myTheme.media.mobile} {
-    max-width: 100%;
-  }
-`;
