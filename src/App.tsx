19dab0f37f4556f841b994a78cba39f45d381663
Zam <jcuccllhfppyxoyddfdlhbh@gmail.com>
2024-04-18 23:01:11 +0300

    feat: add three fiber library, but without used this library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { ThemeProvider } from 'styled-components';
-import { myTheme } from './styles/global/MyTheme.styled';
-import { Wrapper } from './layout/Wrapper.styled';
-import { Header } from './layout/header/Header';
-import { Main } from './layout/sections/Main';
-import { Footer } from './layout/footer/Footer';
-import { Particle } from './components/particle/Particle';
+import { ThemeProvider } from "styled-components";
+import { myTheme } from "./styles/global/MyTheme.styled";
+import { Wrapper } from "./layout/Wrapper.styled";
+import { Header } from "./layout/header/Header";
+import { Main } from "./layout/sections/Main";
+import { Footer } from "./layout/footer/Footer";
+import { Particle } from "./components/particle/Particle";
 
 const App = () => {
   return (
     <div className="App">
       <ThemeProvider theme={myTheme}>
         <Wrapper>
-          <Particle/>
-          <Header/>
-          <Main/>
-          <Footer/>
+          <Particle />
+          <Header />
+          <Main />
+          <Footer />
         </Wrapper>
       </ThemeProvider>
     </div>
   );
-}
+};
 
-export default App
+export default App;
